81aaead196abc75673e0dd0c6195f5cf78cca36f
Sudhanshu <sudhanshu11a@gmail.com>
2016-06-29 01:36:32 +0530

    xml parser added

--- src/main/java/resourcingproblem/spring/parser/XMLParser.java
+++ src/main/java/resourcingproblem/spring/parser/XMLParser.java
@@ -3,10 +3,39 @@
  */
 package resourcingproblem.spring.parser;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.basic.BooleanConverter;
+import com.thoughtworks.xstream.converters.basic.DateConverter;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
+
+import resourcingproblem.spring.model.CompanyOpenings;
+import resourcingproblem.spring.xstream.Opening;
+
 /**
  * @author sudhanshusharma
  *
  */
 public class XMLParser {
 
+	
+	/**
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+//	public <T ? Object>CompanyOpenings populateOpenings() throws FileNotFoundException {
+//		FileReader reader = new FileReader("E:\\contest\\data_june14\\openings.xml"); // load
+//		XStream xstream = new XStream(new StaxDriver());
+//		xstream.registerConverter(new DateConverter("dd-MM-yyyy", new String[] {}));
+//		xstream.registerConverter(new BooleanConverter("Y", "N", false));
+//		xstream.processAnnotations(CompanyOpenings.class); // inform XStream to
+//															// parse annotations
+//															// in Data
+//		xstream.processAnnotations(Opening.class); // and in two other
+//													// classes...
+//		CompanyOpenings data = (CompanyOpenings) xstream.fromXML(reader); // parse
+//		return data;
+//	}
 }
